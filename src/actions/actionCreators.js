e0727f5e7770d76832151260e4d9708d7b7de59d
Aaryashivani <sundropdiamond@gmail.com>
2022-02-18 14:55:54 +0530

    Set up basic redux structure and created redux store

---
+++ src/actions/actionCreators.js
@@ -0,0 +1,9 @@
+export const getLatestArticles = () => {
+    return async (dispatch) => {
+        try {
+            // fetch data from api, dispatch action with payload data
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+}
---
+++ src/reducers/reducer.js
@@ -0,0 +1,8 @@
+const reducer = (state = [], action) => {// state consists of posts here 
+    switch (action.type) {
+        case GET_LATEST_ARTICLES:
+        default: return state;
+    }
+}
+
+export default reducer;
