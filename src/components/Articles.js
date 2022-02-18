d3e4f22d28b791ffe88147e039bf43e976db0209
Aaryashivani <sundropdiamond@gmail.com>
2022-02-18 15:22:57 +0530

    Created component to display articles

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,6 +1,19 @@
+import { useState } from "react";
+
 function Articles() {
+    const [loadedArticles, setLoadedArticles] = useState([{ "field1": "loading", "hours": 0, "minutes": 0, "seconds": 0 }, { "field1": "loading2" }]);
     return <div>
-        Articles
+        {loadedArticles.map((article) => {
+            console.log(article.hours ? article.hours : null);
+            return <div key={article.field1}>
+                <ul>
+                    <li>{article.field1}</li>
+                    <li>{article.hours}</li>
+                    <li>{article.minutes}</li>
+                    <li>{article.seconds}</li>
+                </ul>
+            </div>;
+        })}
     </div>
 }
 
