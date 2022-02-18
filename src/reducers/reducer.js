92c638e3295aba90fdd8d52b47c83911cad7b239
Aaryashivani <sundropdiamond@gmail.com>
2022-02-18 15:23:50 +0530

    Modified initial state for reducer

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,4 +1,11 @@
-const reducer = (state = [], action) => {// state consists of posts here 
+const initialState = {
+    articles: [
+        { "field1": "Random data on frontend", "hours": 1, "minutes": 1, "seconds": 1 },
+        { "field1": "loading2" }
+    ]
+}
+
+const reducer = (state = initialState, action) => {// state consists of posts here 
     switch (action.type) {
         case GET_LATEST_ARTICLES:
         default: return state;
