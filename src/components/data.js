f0e7ae90cc486ea1ce2c2662247e9b24b8710b79
christima <christimamary@gmail.com>
2022-02-25 18:46:42 +0530

    setting up data for navigation bar

---
+++ src/components/data.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const links = [
+  {
+    id: 1,
+    path: "/dashboard",
+    text: "Dashboard",
+  },
+  {
+    id: 2,
+    path: "/login",
+    text: "Login",
+  },
+  {
+    id: 3,
+    path: "/register",
+    text: "Register",
+  },
+];
