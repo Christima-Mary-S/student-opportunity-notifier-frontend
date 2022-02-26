26339c661f4f020a690c3fe29b9a48b52b0b7261
christima <christimamary@gmail.com>
2022-02-26 21:38:41 +0530

    hero text with typewriter

--- src/components/HeroText.js
+++ src/components/HeroText.js
@@ -1,9 +1,24 @@
 import React from "react";
+import Typewriter from "react-simple-typewriter";
 
 export const HeroText = () => {
   return (
-    <div>
-      <h1 className="text-center text-red-400">Hero</h1>
+    <div className="m-4">
+      <h1 className=" text-4xl mb-2">
+        Hello, <br /> Wanna to read some cool articles about
+      </h1>
+      <span className="text-cyan-300 text-6xl">
+        <Typewriter
+          loop
+          cursor
+          cursorStyle="|"
+          typeSpeed={70}
+          deleteSpeed={50}
+          delaySpeed={1000}
+          words={["Internships ?", "Placements ?"]}
+        />
+      </span>
+      <button className="btn">explore now</button>
     </div>
   );
 };
