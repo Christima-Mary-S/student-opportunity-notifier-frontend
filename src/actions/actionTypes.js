46ec7bbb6f7f80ae2ca3c62bef204e00520e5b2a
viola-star <sundropdiamond@gmail.com>
2022-02-27 14:41:32 +0530

    Added action types for getting register/login errors from backend, setting user as loading and setting current user

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,1 +1,4 @@
 export const GET_LATEST_ARTICLES = "GET_LATEST_ARTICLES";
+export const GET_ERRORS = "GET_ERRORS";
+export const USER_LOADING = "USER_LOADING";
+export const SET_CURRENT_USER = "SET_CURRENT_USER";
