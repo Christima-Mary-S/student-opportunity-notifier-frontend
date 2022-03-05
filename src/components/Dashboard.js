048f687442b358a8f0f57494d72069ab49214e82
viola-star <sundropdiamond@gmail.com>
2022-03-05 16:29:59 +0530

    Added selectors for authenticated state in components

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,22 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { createSelector } from 'reselect';
+
+const selectIsAuthenticated = createSelector(
+  state => state.auth,
+  auth => auth.isUserAuthenticated
+);
+
+const selectUser = createSelector(
+    state => state.auth,
+    auth => auth.user
+);
 
 export const Dashboard = () => {
+  const isAuthenticated = useSelector(selectIsAuthenticated);
+  const user = useSelector(selectUser);
+  console.log(user);
+
   return (
     <div>
       <h1 className="text-center text-red-400">Dashboard</h1>
