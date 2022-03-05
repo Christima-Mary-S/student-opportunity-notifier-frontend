60c03421d4dd1da750045b008e10e11b06323968
viola-star <sundropdiamond@gmail.com>
2022-03-05 16:47:59 +0530

    Added SET_CURRENT_USER action dispatch in register function as well

--- src/actions/actionCreators.js
+++ src/actions/actionCreators.js
@@ -18,6 +18,10 @@ export const register = (user) => {
     return async (dispatch) => {
         try {
             const { data } = await api.registerUser(user);
+            dispatch({
+                type: SET_CURRENT_USER,
+                payload: data
+            });
         } catch (error) {
             dispatch({
                 type: GET_ERRORS,
