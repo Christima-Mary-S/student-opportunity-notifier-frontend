73c2b52db7090f7a3c71536c6a170d1091526d4a
christima <christimamary@gmail.com>
2022-03-10 10:20:39 +0530

    changing color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,9 @@ module.exports = {
       colors: {
         "black-dark": "#0B0C10",
         "black-light": "#1F2833",
+        "black-comp": "#1d252e",
         "gray-dark": "#C5C6C7",
-        "cyan-light": "#66FCF1",
+        "cyan-light": "	#1DA1F2",
         "cyan-dark": "#45A295",
       },
       backgroundImage: {
