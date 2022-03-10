f6769a7f835cbac0983bf716491a99fc2454c5a8
viola-star <sundropdiamond@gmail.com>
2022-03-10 11:43:21 +0530

    Removed unnecessary selectors from navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,17 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import { FaBars } from "react-icons/fa";
 import { links } from "./data";
-import { useSelector } from "react-redux";
-import { createSelector } from 'reselect';
-
-const selectIsAuthenticated = createSelector(
-  state => state.auth,
-  auth => auth.isUserAuthenticated
-);
 
 export const NavBar = () => {
-  const isAuthenticated = useSelector(selectIsAuthenticated);
-  
   return (
     <div className="nav-container flex justify-between h-24 items-center m-2 p-6 bg-transparent text-gray-300 shadow-lg">
       <Link to={"/"}>
