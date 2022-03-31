b7cb010db2a1afaea3939611cb284adaa5cf0b6e
christima <christimamary@gmail.com>
2022-03-31 10:25:46 +0530

    initial set up of tabs component on dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
-import { selectIsAuthenticated, selectisAdmin, selectUser } from "../selectors/selectors";
+import {
+  selectIsAuthenticated,
+  selectisAdmin,
+  selectUser,
+} from "../selectors/selectors";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 
 import { LogoutButton } from "./LogoutButton.js";
 import SavedArticles from "./SavedArticles";
-import { getSavedArticles } from '../actions/actionCreators.js';
+import { getSavedArticles } from "../actions/actionCreators.js";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 
 export const Dashboard = () => {
   const dispatch = useDispatch();
@@ -21,12 +26,22 @@ export const Dashboard = () => {
     dispatch(getSavedArticles(user.id));
   }, [dispatch, user]);
 
-  return isAuthenticated? (
-    !isAdmin ? (
-      <div>
-        <h1 className="text-center text-red-400">Dashboard</h1>
-        <SavedArticles />
-      </div>
-    ) : <LogoutButton />
-  ): <h1 className="text-center text-red-400">You are not logged in</h1>;
+  return isAuthenticated ? (
+    <div>
+      <Tabs className="flex  h-96 w-96 md:w-[1000px] md:h-[500px] bg-black-light capitalize text-gray-dark shadow-xl shadow-slate-600">
+        <TabList className="mr-5 md:mr-8">
+          <Tab className="cursor-pointer">Profile</Tab>
+          <Tab className="cursor-pointer">Saved</Tab>
+        </TabList>
+        <TabPanel>
+          <h1>profile component</h1>
+        </TabPanel>
+        <TabPanel>
+          <h1>Saved Articles component</h1>
+        </TabPanel>
+      </Tabs>
+    </div>
+  ) : (
+    <h1 className="text-center text-red-400">You are not logged in</h1>
+  );
 };
