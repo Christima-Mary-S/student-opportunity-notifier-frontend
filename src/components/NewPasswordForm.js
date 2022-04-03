f9d5b33577ccd10ca5f69ec09cd617e716abb812
viola-star <sundropdiamond@gmail.com>
2022-04-03 18:46:52 +0530

    Completed new password form UI

--- src/components/NewPasswordForm.js
+++ src/components/NewPasswordForm.js
@@ -1,3 +1,33 @@
+import { useState } from "react";
+
 export const NewPasswordForm = () => {
-    return <h1>Enter new password</h1>
+    const [password, setPassword] = useState("");
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        console.log(password);
+    };
+
+    return <div>
+    <div className="register-container bg-black-light p-10 capitalize shadow-xl shadow-slate-600  rounded-md hover:scale-105 transform ease-in-out cursor-pointer">
+      <h2 className="mb-2 text-center text-2xl">Update Password</h2>
+      <div className="h-2 bg-cyan-light rounded-t-md mb-2"></div>
+      <form action="" className="form-login">
+        <label htmlFor="password">New password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <div className="flex items-center justify-center">
+          <button type="submit" className="btn" onClick={onSubmit}>
+            Update password
+          </button>
+        </div>      
+      </form>
+    </div>
+  </div>;
 }
