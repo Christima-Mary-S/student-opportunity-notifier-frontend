5f9d7aeefc3e92bf4a455cdfac26f7976477c3cd
christima <christimamary@gmail.com>
2022-04-03 22:51:49 +0530

    Add delete user button

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { selectUsers, selectisAdmin } from "../selectors/selectors";
 import { getUsers } from "../actions/actionCreators";
 
-const columns = ["Name", "Username", "Email", "College", "YoG"];
+const columns = ["Name", "Username", "Email", "College", "YoG", "Action"];
 
 export const Admin = () => {
   const isAdmin = useSelector(selectisAdmin);
@@ -18,7 +18,7 @@ export const Admin = () => {
     console.log("Users are", users);
     return (
       <div className="flex justify-center h-96 w-96 md:w-[1000px] md:h-[500px] bg-black-light capitalize text-gray-dark shadow-xl shadow-slate-600 p-10">
-        <table className="border-2 border-slate-600">
+        <table className="table border-2 border-slate-600 overflow-scroll">
           <thead>
             <tr>
               {columns.map((col) => (
@@ -34,6 +34,11 @@ export const Admin = () => {
                 <td>{user.email}</td>
                 <td>{user.collegeName}</td>
                 <td>{user.yearOfGraduation}</td>
+                <td>
+                  <button className="bg-slate-600 hover:bg-red-600 transform ease-in-out flex justify-center items-center rounded-md border-0 capitalize">
+                    Delete
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
