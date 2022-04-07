2783f3f483cdab8f723f6f7268024f07052a1074
christima <christimamary@gmail.com>
2022-04-07 21:45:34 +0530

    fixed delete button alignment

--- src/components/SavedArticles.js
+++ src/components/SavedArticles.js
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 import { selectSavedArticles } from "../selectors/selectors";
-import { ArticleDeleteButton } from './ArticleDeleteButton';
+import { ArticleDeleteButton } from "./ArticleDeleteButton";
 
 function SavedArticles() {
   const savedArticles = useSelector(selectSavedArticles);
@@ -15,10 +15,17 @@ function SavedArticles() {
             <p>
               {title}, {location}
             </p>
-            <ArticleDeleteButton  key={link} title={title} link={link} location={location} />
-            <button className="bg-cyan-light w-12 transform ease-in-out flex justify-center items-center p-2  rounded-md border-0 capitalize">
-              <a href={link}>Read</a>
-            </button>
+            <div className="flex gap-2">
+              <ArticleDeleteButton
+                key={link}
+                title={title}
+                link={link}
+                location={location}
+              />
+              <button className="bg-cyan-light w-112 transform ease-in-out flex justify-center items-center p-2  rounded-md border-0 capitalize">
+                <a href={link}>Read</a>
+              </button>
+            </div>
           </div>
         );
       })}
