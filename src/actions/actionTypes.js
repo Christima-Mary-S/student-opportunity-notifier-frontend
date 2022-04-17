321d95caf51441c9f2c0c8d3e808c13da1cf991c
viola-star <sundropdiamond@gmail.com>
2022-04-17 11:21:24 +0530

    Debugged update user

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -4,6 +4,7 @@ export const CLEAR_ERRORS = "CLEAR_ERRORS";
 export const SET_ADMIN = "SET_ADMIN";
 export const UNSET_ADMIN = "UNSET_ADMIN";
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
+export const MODIFY_USER = "MODIFY_USER";
 export const SAVE_ARTICLE = "SAVE_ARTICLE";
 export const GET_SAVED_ARTICLES = "GET_SAVED_ARTICLES";
 export const GET_USERS = "GET_USERS";
